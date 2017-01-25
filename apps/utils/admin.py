87280e97f7b7e5275d590536e2f1323dbd6c3572
Hélio Augusto <heliotec2008@gmail.com>
2017-01-25 21:25:21 -0200

    Criado em utils.model Estado e Cidade

--- apps/utils/admin.py
+++ apps/utils/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Regiao
+from .models import Estado
+from .models import Cidade
 
 admin.site.register(Regiao)
+admin.site.register(Estado)
+admin.site.register(Cidade)
 
 # Register your models here.
--- apps/utils/models.py
+++ apps/utils/models.py
@@ -6,6 +6,28 @@ class Regiao(models.Model):
     nome = models.CharField(max_length=32, unique=True)
     sigla = models.CharField(max_length=2, unique=True)
     
+    class Meta:
+        verbose_name = "Região"
+        verbose_name_plural = "Regiões"
+    
     def __str__(self):
         return "{nome}({sigla})".format(nome=self.nome,sigla=self.sigla)
+   
+    
+class Estado(models.Model):
+    nome = models.CharField(max_length=32, unique=True)
+    sigla = models.CharField(max_length=2, unique=True)
+    regiao = models.ForeignKey(Regiao)
+    
+    def __str__(self):
+        return "{nome}".format(nome=self.nome)
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=32, unique=True)
+    ddd = models.CharField(max_length=2, unique=True)
+    estado = models.ForeignKey(Estado)
+        
+    def __str__(self):
+        return "{nome}({estado})".format(nome=self.nome, estado=self.estado)
         
